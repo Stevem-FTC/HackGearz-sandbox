eca5e7c94b9e4e3e1e34ba86db35be009a9100ab
Stevem-FTC <37776035+Stevem-FTC@users.noreply.github.com>
2018-04-08 17:26:22 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * HackGearz startup Java code
+ */
+package helloworld;
+
+/**
+ *
+ * @author steve
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
